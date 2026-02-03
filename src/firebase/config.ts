4d9164479dc58cf5b3306575aa4e13711ed9d682
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2026-02-03 12:33:57 +0000

    // For Firebase JS SDK v7.20.0 and later, measurementId is optional cons

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,13 +1,13 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBkcdN4P3xLedgQ2qU2ENtFuQIAYJ8ExuM",
-  authDomain: "averzo-home.firebaseapp.com",
-  databaseURL: "https://averzo-home-default-rtdb.firebaseio.com",
-  projectId: "averzo-home",
-  storageBucket: "averzo-home.firebasestorage.app",
-  messagingSenderId: "186569132518",
-  appId: "1:186569132518:web:e9109729290e7ee2326e27",
-  measurementId: "G-CXH22NLTQ3"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 export default firebaseConfig;
